80b74870dc12a289f0d60f9d75d7d96fa9be2c0f
DurlaVerder <vrrrr227@gmail.com>
2024-11-19 01:35:34 +0300

    Add init data base

--- internal/repository/postgres/postgres.go
+++ internal/repository/postgres/postgres.go
@@ -2,10 +2,29 @@ package postgres
 
 import (
 	"database/sql"
+	"log"
 
 	_ "github.com/lib/pq"
 )
 
 type PostgresRepo struct {
 	db *sql.DB
+}
+
+func NewPostgresRepo() *PostgresRepo {
+	return PostgresRepo(initDB)
+}
+
+func initDB() *sql.DB {
+	open := "user=postgres password=durka dbname=Expense-Tracker sslmode=disable"
+	db, err := sql.Open("postgres", open)
+	if err != nil {
+		log.Fatal("Error open db: ", err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal("Error connect db: ", err)
+	}
+
+	return db
 }
