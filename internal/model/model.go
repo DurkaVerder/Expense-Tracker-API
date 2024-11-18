89a7a16af50c3369f1d51afaab7368a5d077348f
DurlaVerder <vrrrr227@gmail.com>
2024-11-19 01:04:28 +0300

    Update all interfaces

--- internal/model/model.go
+++ internal/model/model.go
@@ -1,8 +1,18 @@
 package model
 
+import "time"
+
 type User struct {
-	Id       int
-	Name     string
-	Login    string
-	Password string
+	Id       int    `json:"id"`
+	Name     string `json:"name"`
+	Login    string `json:"login"`
+	Password string `json:"password"`
+}
+
+type Expense struct {
+	Id          int        `json:"id"`
+	Title       string     `json:"title"`
+	Cost        int        `json:"cost"`
+	CreatedData *time.Time `json:"created_data"`
+	CreatorId   int        `json:"creator_id"`
 }
