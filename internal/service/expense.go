1a5dc8c2c7a8edc0f1e08f300cf9f823322c09d1
DurlaVerder <vrrrr227@gmail.com>
2024-11-20 15:04:30 +0300

    Update service

--- internal/service/expense.go
+++ internal/service/expense.go
@@ -2,18 +2,10 @@ package service
 
 import "Expense-Tracker-API/internal/model"
 
-func (s *ServiceManager) GetExpenses(userId int) ([]model.Expense, error) {
-	
-}
+func (s *ServiceManager) GetExpenses(userId int) ([]model.Expense, error) 
 
-func (s *ServiceManager) AddExpense(data model.Expense) error {
+func (s *ServiceManager) AddExpense(data model.Expense) error 
 
-}
+func (s *ServiceManager) UpdateExpense(data model.Expense) error 
 
-func (s *ServiceManager) UpdateExpense(data model.Expense) error {
-
-}
-
-func (s *ServiceManager) DeleteExpense(expenseId int) error {
-
-}
+func (s *ServiceManager) DeleteExpense(expenseId int) error
